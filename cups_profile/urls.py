3ea2bb20fcc98982ba1adcfecd41f0caf920a02d
animasola <annima.sola@gmail.com>
2014-07-23 23:20:49 +0300

    ticket:1 Trying to fix static problem on getbarista

--- cups_profile/urls.py
+++ cups_profile/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +10,12 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
 )
