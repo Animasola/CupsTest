902939f84c8fce24f3ebd13d5ff73e25eeb44bd5
animasola <annima.sola@gmail.com>
2014-07-24 18:33:33 +0300

    ticket:3 Middleware ready and working

--- my_info/middleware.py
+++ my_info/middleware.py
@@ -1,1 +1,14 @@
- 
+#-*- coding:utf-8 -*-
+from my_info.models import LoggedRequest
+
+
+class RequestLogger(object):
+    def process_request(self, request):
+        try:
+            LoggedRequest.objects.create(**{
+                'ip': request.META['REMOTE_ADDR'],
+                'request_type': request.method,
+                'url': request.build_absolute_uri(request.path)
+            })
+        except:
+            raise Exception("Failed to log request.")
